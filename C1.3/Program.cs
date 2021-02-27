b8bd472eb8b20125f83fa4f451335ebb51eec8b7
Moldovan Tudor <tmoldovan19@yahoo.com>
2021-02-27 12:04:44 +0200

    Add project files.

---
+++ C1.3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace C1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double g = 9.81, d = 0, dd;
+            int t = 0;
+
+            Console.Write("Dati distanta maxima ");
+                try
+            {
+                dd = int.Parse(Console.ReadLine());
+                while (d<=dd )
+                {
+                    d = (g * t * t) / 2;
+                    Console.WriteLine(t.ToString() + " " + d);
+                    t += 1;
+                }
+            }
+
+                catch (Exception ex)
+              {
+                Console.WriteLine(ex.Message);
+              }
+                Console.ReadLine();
+        }
+        
+          
+    }
+}
